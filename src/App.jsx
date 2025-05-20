618f8529383d0049b632575d868a0ab6af1b7ef2
khs <hs.kwan@okestro.com>
2025-05-20 23:17:53 +0900

    feat: Completed the implementation from data input to receiving a response from GPT

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { callGpt } from "./api/gpt";
+import DiaryInput from "./components/DiaryInput.jsx";
 
 const dummyData = JSON.parse(
   `{ "title": "개발 고민과 해결", "thumbnail": "https://source.unsplash.com/1600x900/?coding", "summary": "코딩 강의를 듣고 프로젝트에 버그가 발생했지만 해결하지 못하여 GPT를 통해 문제를 해결했음", "emotional_content": "오늘 코딩 강의를 들었는데, 프로젝트에 버그가 많이 나왔어. 스택오버플로에서 검색해봤지만 해결되지 않았어. 그래서 결국 GPT를 통해서 문제를 해결하게 되었어. 하지만 이렇게 해결하는 것이 내 개발 실력에 도움이 될까 고민이 되는군.", "emotional_result": "이번 상황을 통해 내가 프로그래밍에 대해 더 배울 필요가 있음을 느꼈다. 버그를 해결하는 데에만 의존하는 것보다 개념적으로 이해하고 해결하는 것이 더 중요하다는 것을 깨달았다.", "analysis": "이번 상황은 개발자로서 성장하는 과정에서 마주치는 문제였다. 알고리즘과 문제 해결 능력은 중요하지만, 개념적인 이해와 전체적인 시스템 구조 파악이 더 중요하다는 것을 알 수 있었다. '지식은 힘이다'라는 명언을 생각해보면, 기술적인 도움을 받는 것도 중요하지만 개념적인 이해와 학습은 더 큰 힘이 될 것이다.", "action_list": ["더 깊은 개념적 이해를 위해 관련 서적을 읽어보기", "다른 개발자들과 소통하여 문제 해결 방법 나누기", "개발자 커뮤니티에 참여하여 지식을 공유하기"] }`
@@ -9,16 +10,13 @@ function App() {
   const [data, setData] = useState(dummyData);
   const [isLoading, setLoading] = useState(false);
 
-  const hadndleClickApiCall = async () => {
-    setLoading(true);
+  const hadndleClickApiCall = async (userInput) => {
     try {
+      setLoading(true);
       const message = await callGpt({
-        prompt: `
-        코딩 강의를 들었다. 프로젝트에 버그가 많이 나왔음. 스택오버플로에서 검색했지만 해결 안되었어.
-역시 gpt를 통해서 해결했다. 근데 이렇게 해결하는게 개발실력에 도움 될까..?
-        `,
+        prompt: `${userInput}`,
       });
-      setData(message);
+      setData(JSON.parse(message));
     } catch (e) {
       console.log(e);
     } finally {
@@ -26,8 +24,13 @@ function App() {
     }
   };
 
+  function submitHandler(userInput) {
+    hadndleClickApiCall(userInput);
+  }
+
   return (
     <>
+      <DiaryInput isLoading={isLoading} onSubmit={submitHandler} />
       <div>data: {JSON.stringify(data)}</div>
       <div>isLoaidng: {isLoading ? "loading" : "fin"}</div>
       <button onClick={hadndleClickApiCall}>gpt call</button>
